c747a8b6647351531db5ca1c844a1a744d86bee0
ketcomp <gketkar08@gmail.com>
2016-02-10 14:45:30 -0600

    Homework2 complete

--- homework2/bigramSearch.py
+++ homework2/bigramSearch.py
@@ -3,22 +3,28 @@ import pprint
 from collections import Counter
 
 
-# Split text at spaces and return a list of words
+# Split text at spaces
+# Return a list of words
 def splitAtSpaces(fileToSplit) -> list:
     listOfWords = fileToSplit.split()
     return listOfWords
 
-# Group words into bigrams and return a list of those bigrams
+
+# Group words into bigrams
+# Return a list of those bigrams
 def wordsToBigrams(words) -> list:
     bigrams = [(words[element - 1], words[element]) for element in range(1, (words.__len__()))]
     return bigrams
 
-# Tally the # of occurrences of bigrams and return a dictionary counting occurrences of bigrams
+
+# Tally the # of occurrences of bigrams
+# Return a dictionary counting occurrences of bigrams
 def countBigrams(bigrams) -> dict:
     bigramOccurrences = Counter()
     for bigram in bigrams:
         bigramOccurrences[bigram] += 1
     return bigramOccurrences
+
 
 # How many times have the bigrams in the given sentences appeared in the corpus?
 def sentenceBigramsCounts(sentenceBigrams, bigramOccurances):
@@ -27,7 +33,9 @@ def sentenceBigramsCounts(sentenceBigrams, bigramOccurances):
         result.update({sentenceBigrams[b]: bigramOccurrences[sentenceBigrams[b]]})
     return result
 
-# Tally # of occurrences of words (or Unigrams) and return a dictionary containing the #'s
+
+# Tally # of occurrences of words (or Unigrams)
+# Return a dictionary containing the #'s
 def countWords(words):
     vocabulary = Counter()
     for word in words:
@@ -36,6 +44,7 @@ def countWords(words):
     # pprint.pprint(vocabulary)
     # print(vocabularySize)
     return vocabulary, vocabularySize
+
 
 # Generate the bigram count table for sentence1 and sentence2
 def bigramCountTable(sentence1, sentence2, bigrams):
@@ -67,23 +76,117 @@ def bigramCountTable(sentence1, sentence2, bigrams):
     f.close()
     g.close()
 
-# Case 1: No Smoothing. Calculate the probabilities of the sentences.
+
+# Case 1: No Smoothing.
+# Calculate the probabilities of the sentences.
 def noSmoothing(bigTallyInFirst, bigTallyInSecond, vocabulary, bigramOccurrences):
     print("For the case 1 : No smoothing applied")
     print("*************************************")
-    # Initialize probabilities to 0
-    sentence1Prob, sentence2Prob = 0, 0
-    # Sentence 1
-    # Sentence 2
 
-# Case 2 : Add one smoothing. Calculate Add-one smoothing
+    def calculations(bigTallyInSentence, sentenceNo):
+        # Initialize probability to 0
+        # print(bigramOccurrences[])
+        sentenceProb = 0
+        print("Sentence " + str(sentenceNo))
+        for key, value in bigTallyInSentence.items():
+            # print(value, vocabulary[str(key[0])])
+            if value == 0:
+                print("The probability of sentence  is ZERO")
+                return 0
+            sentenceProb += (math.log(value) - math.log(vocabulary[str(key[0])]))
+        if sentenceProb != 0:
+            print("The probability of sentence is: " + str(math.exp(sentenceProb)))
+            return sentenceProb
+
+    # Sentence 1
+    sentence1Prob = calculations(bigTallyInFirst, 1)
+    # print(vocabulary)
+    # Sentence 2
+    sentence2Prob = calculations(bigTallyInSecond, 2)
+
+
+# Case 2 : Add one smoothing.
+# Calculate Add-one smoothing
 def addOneSmoothing(bigramsInFirst, bigramsInSecond, vocabulary, bigramOccurances):
+    print()
     print("For add-one smoothing:")
     print("**********************")
-    # Initialize probabilities to 0
-    sentence1Prob, sentence2Prob = 0, 0
+
+    def addOneCalculations(bigTallyInSentence, sentenceNo, vocabulary):
+        # Initialize probability to 0
+        sentenceProb = 0
+        print("Sentence " + str(sentenceNo))
+        f = open('s' + str(sentenceNo) + 'AddOne.txt', 'w')
+        for key, value in bigTallyInSentence.items():
+            sentenceProb += (math.log(value + 1) - math.log(vocabulary[str(key[0])] + vocabulary.__len__()))
+            f.write(str(
+                (key, str(math.exp(math.log(value + 1) - math.log(vocabulary[str(key[0])] + vocabulary.__len__()))))))
+        f.close()
+        print("The probability of sentence is: " + str(math.exp(sentenceProb)))
+        return math.exp(sentenceProb)
+
     # Sentence1
+    sentence1Prob = addOneCalculations(bigTallyInFirst, 1, vocabulary)
     # Sentence2
+    sentence2Prob = addOneCalculations(bigTallyInSecond, 2, vocabulary)
+    # Conclusion
+    conclusion = "Sentence 1" if (sentence1Prob > sentence2Prob) else "Sentence 2"
+    print()
+    print("The most probable sentence is " + conclusion)
+
+
+# Calculate the frequency of frequencies of corpus bigrams needed for Good-Turing approx.
+def freqOfFreq(bigramsOccurrences):
+    bigramOccurrences = dict(bigramsOccurrences)
+    # print(bigramOccurrences)
+    keepCount = {}
+
+    for k in range(0, 300):  # Arbitrary, large range to store the freq of freqencies.
+        keepCount.update({k: 0})
+    i = 0
+    for keys, values, in bigramOccurrences.items():
+        keepCount[bigramOccurrences[keys]] += bigramOccurrences[keys]
+    # pprint.pprint(keepCount)
+    return keepCount
+
+
+# Case 3 : Good-Turing discounting.
+def goodTuring(bigTallyInFirst, bigTallyInSecond, vocabulary, bigramOccurrences, keepCount):
+    print()
+    print("For Good-Turing smoothing:")
+    print("**********************")
+
+    def goodTuringCalculations(bigTallyInSentence, sentenceNo, vocabulary):
+        # Initialize probability to 0
+        sentenceProb = 0
+        # print(keepCount)
+
+        f = open('s' + str(sentenceNo) + 'GT.txt', 'w')
+        for key, value in bigTallyInSentence.items():
+            if (0 == bigramOccurrences[key]):
+                sentenceProb += (math.log(keepCount[1]) - math.log(bigrams.__len__()))
+                f.write(str(
+                    (key, str((math.log(keepCount[1]) - math.log(bigrams.__len__()))))))
+            elif (bigramOccurrences[key] > 5):
+                sentenceProb += (math.log(value + 1) - math.log(vocabulary[str(key[0])] + vocabulary.__len__()))
+                f.write(str(
+                    (key,
+                     str(math.exp(math.log(value + 1) - math.log(vocabulary[str(key[0])] + vocabulary.__len__()))))))
+
+        f.close()
+
+        print("The probability of sentence " + str(sentenceNo) + " is: " + str(math.exp(sentenceProb)))
+        return math.exp(sentenceProb)
+
+    # Sentence1
+    sentence1Prob = goodTuringCalculations(bigTallyInFirst, 1, vocabulary)
+    # Sentence2
+    sentence2Prob = goodTuringCalculations(bigTallyInSecond, 2, vocabulary)
+    # Conclusion
+    conclusion = "Sentence 1" if (sentence1Prob > sentence2Prob) else "Sentence 2"
+    print()
+    print("The most probable sentence is " + conclusion)
+
 
 if __name__ == '__main__':
     # Read corpus data in the variable corpusFile
@@ -96,7 +199,7 @@ if __name__ == '__main__':
 
     # Form bigrams from all the 'words'
     bigrams = wordsToBigrams(words)
-    # pprint.pprint(bigrams)
+    # pprint.pprint(bigrams.__len__())
 
     # Tally the bigrams in the corpus
     bigramOccurrences = countBigrams(bigrams)
@@ -120,5 +223,9 @@ if __name__ == '__main__':
 
     # Calculate the answers
     noSmoothing(bigTallyInFirst, bigTallyInSecond, vocabulary, bigramOccurrences)
-    # addOneSmoothing(bigramsInFirst, bigramsInSecond, vocabulary)
-    # print("Probabilities of sentence one")
+
+    addOneSmoothing(bigTallyInFirst, bigTallyInSecond, vocabulary, bigramOccurrences)
+
+    # The tally needed for Good-Turing
+    freqOfFreq = freqOfFreq(bigramOccurrences)
+    goodTuring(bigTallyInFirst, bigTallyInSecond, vocabulary, bigramOccurrences, freqOfFreq)
